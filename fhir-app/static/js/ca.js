675c3798c1298e5b7fb0e1ef45060b3b213896fc
Andrew Cullen <drewcullen@0587359968.wireless.umich.net>
2017-02-17 13:19:31 -0500

    SMART cardiac advisor

---
+++ fhir-app/static/js/ca.js
@@ -0,0 +1,141 @@
+
+$(document).ready(function()
+{
+	
+	$("#get_data").click(function()
+	{
+	  //alert('yo');
+		FHIR.oauth2.ready(function(smart)
+		{
+		   // alert("whoa")
+			var patient = smart.patient.read()
+			$.when(patient).done(function(pt)
+			{
+				console.log("PATIENT RESOURCE: ", pt);
+			   // alert('got yo patient')
+				patientInfo = pt;
+				console.log(patientInfo);
+
+				get_ischemic_data(pt);
+				get_stent_data();
+				$(".visual-field").slideDown("slow");
+
+			})
+		})
+
+	})
+
+	$("input:radio[name='yes/no']").change(function()
+	{
+		//alert("!");
+		$("#bleeding-icon").slideUp("slow");
+		$("#bleeding-icon").html("");
+		$("#stent-gage").slideUp("slow");
+		$("#stent-gage").html("");
+		if($("input[name='yes/no']:checked").length == 12)
+		{
+			$("#get_data").slideDown("slow");
+		}
+
+	})
+
+	$(".sample").click(function()
+	{
+		$("#bleeding-icon").slideUp("slow");
+		$("#bleeding-icon").html("");
+		$("#stent-gage").slideUp("slow");
+		$("#stent-gage").html("");
+	})
+
+	$("#sample1").click(function()
+	{
+		$(".no-btn").each(function(index)
+		{
+			if(index % 2)
+			{
+				$(this).prop("checked", true);
+			}
+
+		})
+
+		$(".yes-btn").each(function(index)
+		{
+			if(!(index % 2))
+			{
+				$(this).prop("checked", true);
+			}
+		})
+
+		$("#get_data").slideDown("slow");
+
+	})
+
+	$("#sample2").click(function()
+	{
+		$(".no-btn").each(function(index)
+		{
+			if(!(index % 3))
+				$(this).prop("checked", true);
+		})
+		$(".yes-btn").each(function(index)
+		{
+			if(index % 3)
+				$(this).prop("checked", true);
+		})
+
+		$("#get_data").slideDown("slow")
+	})
+
+	$("#sample3").click(function()
+	{
+		$(".yes-btn").each(function()
+		{
+			if(!$(this).is(":checked"))
+				$(this).prop("checked", true);
+		})
+
+		$("#get_data").slideDown("slow")
+	});
+
+	$("#sample4").click(function()
+	{
+		$(".no-btn").each(function()
+		{
+			if(!$(this).is(":checked"))
+				$(this).prop("checked", true);
+		})
+
+		$("#get_data").slideDown("slow")
+
+	});
+
+
+	$(".show_gage").click(function()
+	{
+		$div = $(this).parent("div");
+		id = $div.attr("id");
+		text = $div.find("span").text();
+		text = text.substr(0, text.length - 1);
+
+		//alert(text);
+		var divID_ = this.name;
+		console.log("divID: ", divID_);
+		var domObj = $("#" + divID_);
+		
+		if(!domObj.is(":visible"))
+		{
+			draw_array({divID: divID_, count: parseFloat(text), gridWidth: 10, gridHeight: 10, personFill: "steelblue",
+					backgroundFill: "#FFFFFF", key: true})
+			$("#" + divID_).slideDown("slow");
+		}
+
+	})
+
+	 $("#start").click(function()
+  	 {
+  		$("#input_div").slideDown("slow");
+  	 })
+	  
+})
+
+ 
---
+++ fhir-app/static/js/helpers.js
@@ -0,0 +1,134 @@
+// var keyDict = {0: 'DAPT', 4: 'infar', 5: 'priorPCI', 6: 'CHF', 7: 'veinGraft', 8: 'stentDiameter', 
+//  				9: 'pac', 10: 'cigSmoker', 11: 'diabetes', 1: 'periphDisease',  2: 'hypertension', 3: 'renal'};
+
+var keyDict = {'DAPT': 0, 'infar': 4, 'priorPCI': 5, 'CHF': 6, 'veinGraft': 7, 'stentDiameter': 8, 
+ 				'pac': 9, 'cigSmoker': 10, 'diabetes': 11, 'periphDisease': 1,  'hypertension': 2, 'renal': 3};
+
+//input: birhtdate as a string in the form YYYY-MM-DD
+//output: an integer representing the age based on the birthdate
+function calculateAge(birthday)
+	{
+		var today = new Date();
+		var birthDate = new Date(birthday);
+		var age = today.getFullYear() - birthDate.getFullYear();
+		var m = today.getMonth() - birthDate.getMonth();
+		if(m < 0 || (m == 0 && today.getDate() < birthDate.getDate()))
+			age--;
+
+		return age;
+	}
+
+//input: patient FHIR resource
+//output: patient's name as a string
+function get_patient_name(patient)
+{
+	if(patient.name)
+	{
+		var names = patient.name.map(function(name)
+		{
+			return name.given.join(" ") + " " + name.family.join(" ");
+		})
+		return names.join("/");
+	}
+	else return "anonymous";
+}
+
+  function getButtonValue(inputName)
+  {
+	  alert($('input[name="yes/no"]:checked').val())
+  }
+  
+  //input: parameeters for knwledge object, takes an arbitrary number of parameters. 
+  //optionals is an optional parameter, it should be an object that holds any extra keys you want to add to data
+  //needs: a key dictionary for mapping values
+  //output: returns object mapping parameters to their values
+  function get_data(args, optionals)
+  {
+  	var data = {};
+  	var checkedValues = $("input[name='yes/no']:checked").map(function()
+  	{
+  		return this.value
+  	}).get()
+
+  	console.log("got checked values: ", checkedValues)
+
+  	for(var i = 0; i < args.length; i += 1)
+  	{
+  		data[args[i]] = checkedValues[keyDict[args[i]]]
+  	}
+  	if(optionals)
+  	{
+  		for(var key in optionals)
+  		{
+  			data[key] = optionals[key];
+  		}
+  	}
+
+  	console.log("populated data: ", data)
+
+  	return JSON.stringify(data);
+  }
+
+//input: data, ark id, success callback, error callback. Parameters need to be in the same object
+//output: sends a POST request to the knowledge object with the ark ID, runs callbacks on success/error (async)
+  function KOPost(instr)
+  {
+  	var set = 
+  	 {
+		  "url": "http://dlhs-fedora-dev-a.umms.med.umich.edu:8080/ExecutionStack/knowledgeObject/ark:/"+ instr.arkID + "/result",
+		  "method": "POST",
+		  "headers": {
+			  "content-type": "application/json",
+		  },
+		  "data": instr.data
+	 }
+
+	 console.log("AJAX SETTINGS: ", set)
+	 $.ajax(set).done(function(response)
+	 {
+	 	if(response.result) instr.success(response);
+	 	else instr.error(response);
+	 })
+  }
+
+  function get_stent_data()
+  {
+  	KOPost(
+  	{
+  		arkID: "99999/fk45m6gq9t",
+  		data: get_data(['DAPT', 'infar', 'hypertension', 'priorPCI', 'CHF', 'veinGraft', 'stentDiameter', 
+  			'pac', 'cigSmoker', 'diabetes', 'periphDisease', 'renal']),
+  		success: function(response)
+  		{
+  			console.log('got value from stent object');
+			$("#stent-risk").text((response.result * 100).toFixed(2) + '%');	
+  		},
+  		error: function(response)
+  		{
+  			console.log('got error in stent msg');
+			console.log(response.errorMessage);
+  		}
+  	})
+  }
+
+function get_ischemic_data(pt)
+{
+	console.log('BIRTHDATE: ', pt.birthDate)
+	KOPost(
+	{
+		arkID: "67034/k47c7m",
+		data: get_data(['DAPT', 'periphDisease', 'hypertension', 'renal'], {age: calculateAge(pt.birthDate)}),
+		success: function(response)
+		{
+			console.log('result  ' + response.result);
+			$("#bleed-risk").text("" + (response.result * 100).toFixed(2) + '%');
+		},
+		error: function(response)
+		{
+			console.log(response.errorMessage);
+		  	$("#bleed-vis").css("display", "none");
+			$("#bleed-risk").text(response.errorMessage);
+		}
+	})
+
+}
