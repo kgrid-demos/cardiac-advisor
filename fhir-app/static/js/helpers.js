801ac4ec404b3092c049670b99ca1ba800fbc0e0
Andrew Cullen <drewcullen@0587366118.wireless.umich.net>
2017-03-20 15:13:33 -0400

    made date in fhir resource to be current date

--- fhir-app/static/js/helpers.js
+++ fhir-app/static/js/helpers.js
@@ -198,13 +198,25 @@ function autofill(num, retrieved)
 
 function write_risk_data(bleedRisk, stentRisk, smart)
 {
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+
+	var yyyy = today.getFullYear();
+	if(dd<10){
+	    dd='0'+dd;
+	} 
+	if(mm<10){
+	    mm='0'+mm;
+	} 
+	var today = yyyy+'-'+mm+'-'+dd;
 	var riskAsm = 
 	{
 		"resource":
 	{
      "resourceType": "RiskAssessment",              
      "id": "kgrid-ra101",                      
-     "date": "2014-07-19",                      
+     "date": today,                      
      "subject":{
        "reference":"Patient/" + smart.patient.id                               
       },
