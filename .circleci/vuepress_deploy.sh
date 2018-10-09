cf2a42d7f17edb0820a75f7b2c1dbce0a38bef46
George Meng <gqmeng.meng@gmail.com>
2018-10-09 15:11:01 -0400

    Enabled circleci to the repo

--- .circleci/vuepress_deploy.sh
+++ .circleci/vuepress_deploy.sh
@@ -8,17 +8,16 @@ git config --global user.name "circleci"
 # build
 npm run docs:build
 # copy web demo to github pages dist
-cd docs
-cp -r fhir-app .vuepress/dist
-# mkdir -p docs/.vuepress/dist/.circleci
-# cp -a .circleci/. docs/.vuepress/dist/.circleci/.
+cp -r app docs/.vuepress/dist
+mkdir -p docs/.vuepress/dist/.circleci
+cp -a .circleci/. docs/.vuepress/dist/.circleci/.
 
 # navigate into the build output directory
-cd .vuepress/dist
+cd docs/.vuepress/dist
 
 git init
 git add -A
 git commit -m "Automated deployment to GitHub Pages"
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://${GITHUB_TOKEN}@github.com/kgrid-demos/cardiac-advisor.git master:gh-pages
+git push -f https://${GITHUB_TOKEN}@github.com/kgrid-demos/cancer-advisor.git master:gh-pages
